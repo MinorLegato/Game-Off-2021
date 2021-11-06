da8c1b47d194f7dabd934bc72fc61f94b8a07310
MinorLegato <anton.siderud@gmail.com>
2021-11-06 09:44:13 +0100

    asdf

--- render.cc
+++ render.cc
@@ -1,27 +1,4 @@
 
-static const gl_shader_desc_t shader_desc = {
-    GLSL_SHADER(
-        layout (location = 0) in vec3 in_position;
-        layout (location = 1) in vec4 in_color;
-
-        out vec4 frag_color;
-
-        uniform mat4 pvm;
-
-        void main() {
-            frag_color = in_color;
-            gl_Position = pvm * vec4(in_position, 1);
-        }),
-
-    GLSL_SHADER(
-        in vec4 frag_color;
-        out vec4 out_color;
-
-        void main() {
-            out_color = frag_color;
-        }),
-};
-
 static gl_shader_t  shader;
 static u32          pvm_location;
 
